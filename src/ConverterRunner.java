227d57c0d0ad75546b937c335b169be5f3159e0f
kazit <ktishan3135@bths.edu>
2023-01-31 19:13:01 -0500

    Worked on input check and changed variable types from int to String

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -6,21 +6,47 @@ class ConverterRunner {
         Scanner s = new Scanner(System.in);
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        int base = 0;
-        int n = 0;
-        String number = "";
-        while (NumberConverter.checkInputs(base, number) == false){
-            System.out.print("Enter the base of your number (2, 8 or 10): ");
-            String choice = s.nextLine();
-            base = Integer.parseInt(choice);
-            System.out.print("Enter your number: ");
-            number = s.nextLine();
-            if (NumberConverter.checkInputs(base, number) == false) System.out.println(number + " is not a number of base " + base);
+        System.out.println("1. Convert a number of base 2, 8, 10, or 16");
+        System.out.println("2. Convert a decimal number to any base from 1 - 64");
+        System.out.print("Enter the mode you would like to choose (1 or 2): ");
+        String choice = s.nextLine();
+        while (!(choice.equals("1") || choice.equals("2"))){
+            System.out.print("Please enter 1 or 2: ");
+            choice = s.nextLine();
         }
-        s.close();
-        NumberConverter nc = new NumberConverter(n, base);
-        System.out.println(nc.toString());
-
+        if (choice.equals("1")){
+            System.out.print("Enter the base of your number (2, 8, 10, or 16): ");
+            String base = s.nextLine();
+            while (!(base.equals("2") || base.equals("8") || base.equals("10") || base.equals("16"))){
+                System.out.print("Please enter a base of 2, 8, 10, or 16: ");
+                base = s.nextLine();
+            }
+            System.out.print("Enter your number: ");
+            String number = s.nextLine();
+            while (NumberConverter.checkInputs(Integer.parseInt(base), number) == false){
+                System.out.println(number + " is not a number of base " + base);
+                System.out.print("Please enter a number of base " + base + ": ");
+                number = s.nextLine();
+            }
+            s.close();
+            NumberConverter nc = new NumberConverter(number, Integer.parseInt(base));
+            System.out.println(nc.toString());
+        }
+        else if (choice.equals("2")){
+            System.out.print("Enter your number: ");
+            String number = s.nextLine();
+            while (NumberConverter.checkInputs(10,number) == false){
+                System.out.print("Please enter a decimal number: ");
+                number = s.nextLine();
+            }
+            System.out.print("Enter the base you would like to convert to (1 - 64): ");
+            String base = s.nextLine();
+            while (NumberConverter.numIsInRange(1, 64, base) == false){
+                System.out.print("Please enter a number from 1 - 64: ");
+                base = s.nextLine();
+            }
+            System.out.println(NumberConverter.convertDecimalToBaseX(Integer.parseInt(number),Integer.parseInt(base)));
+        }
     }
 }
 
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,14 +1,12 @@
 public class NumberConverter {
-    int[] digits;
-    int base;
+    private String[] digits;
+    private int base;
 
-    public NumberConverter(int number, int base) {
-        String numberAsString = Integer.toString(number);
-        digits = new int[numberAsString.length()];
-        for (int i = 0; i < numberAsString.length(); i++) {
-            String single = numberAsString.substring(i,i+1);
-            int d = Integer.parseInt(single);
-            digits[i] = d;
+    public NumberConverter(String number, int base) {
+        digits = new String[number.length()];
+        for (int i = 0; i < number.length(); i++) {
+            String single = number.substring(i,i+1);
+            digits[i] = single;
         }
         this.base = base;
     }
@@ -22,60 +20,50 @@ public class NumberConverter {
         return o;
     }
 
-    public int[] getDigits() {
+    public String[] getDigits() {
         return digits;
     }
 
-    private static int convertListToInt(int[] list){
-        String listString = "";
+    private static String convertStringListToString(String[] list){
+        String numAsString = "";
         for (int i = 0; i < list.length; i++){
-            listString += list[i];
+            numAsString += list[i];
         }
-        int number = Integer.parseInt(listString);
-        return number;
+        return numAsString;
     }
 
-    private static int[] convertStringToList(String string){
-        int[] list = new int[string.length()];
+    private static String[] convertStringToStringList(String string){
+        String[] list = new String[string.length()];
         for (int i = 0; i < string.length(); i++){
             String num = string.charAt(i) + "";
-            int digit = Integer.parseInt(num);
-            list[i] = digit;
+            list[i] = num;
         }
         return list;
     }
 
-    public int[] convertToDecimal() {
+    public String[] convertToDecimal() {
+        String[] newDigits;
+        String values = "0123456789ABCDEF";
         int decimalValue = 0;
         // finding the decimal value of the number:
-        if (base == 2){
-            int value = 1;
-            for (int i = digits.length - 1; i >= 0; i--){
-                decimalValue += value * digits[i];
-                value *= 2;
-            }
-        }
-        if (base == 8){
-            int value = 1;
-            for (int i = digits.length - 1; i >= 0; i--){
-                decimalValue += value * digits[i];
-                value *= 8;
-            }
+        int value = 1;
+        for (int i = digits.length - 1; i >= 0; i--){
+            decimalValue += value * values.indexOf(digits[i]);
+            value *= base;
         }
         // turning the number into a list
         String numberAsString = decimalValue + "";
-        int[] newDigits = convertStringToList(numberAsString);
+        newDigits = convertStringToStringList(numberAsString);
         return newDigits;
     }
 
-    public String convertToBinary() {
+    public String[] convertToBinary() {
         String binaryValue = "";
         // making sure everything in the list is in decimal form:
-        int[] decimalDigits = digits;
-        if (base == 8) decimalDigits = convertToDecimal();
+        String[] decimalDigits = convertToDecimal();
         // turning the list into an int:
-        int decimal = convertListToInt(decimalDigits);
-        // finding the highest value of 2 that can go into decimal:
+        int decimal = Integer.parseInt(convertStringListToString(decimalDigits));
+        // finding the highest power of 2 that can go into decimal:
         int power = 0;
         while (decimal - Math.pow(2, power) >= 0) power++;
         if (decimal - Math.pow(2, power) < 0) power--;
@@ -87,21 +75,22 @@ public class NumberConverter {
             decimal -= Math.pow(2, power) * quotient;
             power--;
         }
-        return binaryValue;
+        // turning binaryValue into a list:
+        String[] binaryDigits = convertStringToStringList(binaryValue);
+        return binaryDigits;
     }
 
-    public String convertToOctal() {
+    public String[] convertToOctal() {
         String octalValue = "";
         // making sure everything in the list is in decimal form:
-        int[] decimalDigits = digits;
-        if (base == 2) decimalDigits = convertToDecimal();
+        String[] decimalDigits = convertToDecimal();
         // turning the list into an int:
-        int decimal = convertListToInt(decimalDigits);
-        // finding the highest value of 8 that can go into decimal:
+        int decimal = Integer.parseInt(convertStringListToString(decimalDigits));
+        // finding the highest power of 8 that can go into decimal:
         int power = 0;
         while (decimal - Math.pow(8, power) >= 0) power++;
         if (decimal - Math.pow(8, power) < 0) power--;
-        // adding the numbers to binaryValue:
+        // adding the numbers to octalValue:
         int quotient;
         while (power >= 0){
             quotient = decimal / (int) Math.pow(8, power);
@@ -109,14 +98,39 @@ public class NumberConverter {
             decimal -= Math.pow(8, power) * quotient;
             power--;
         }
-        return octalValue;
+        // turning octalValue into a list:
+        String[] octalDigits = convertStringToStringList(octalValue);
+        return octalDigits;
+    }
+
+    public static String convertDecimalToBaseX(int decimal, int base){
+        String baseXValue = "";
+        if (base == 1){
+            for (int i = 0; i < decimal; i++){
+                baseXValue += "1";
+            }
+            return baseXValue;
+        }
+        else if (decimal == 0) return "0";
+        String values = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+        // finding the highest power of base X that can go into decimal:
+        int power = 0;
+        while (decimal - Math.pow(base, power) >= 0) power++;
+        if (decimal - Math.pow(base, power) < 0) power--;
+        // adding the characters to baseXValue:
+        int quotient;
+        while (power >= 0){
+            quotient = decimal / (int) Math.pow(base, power);
+            baseXValue += values.substring(quotient, quotient + 1);
+            decimal -= Math.pow(base, power) * quotient;
+            power--;
+        }
+        return baseXValue;
     }
 
     public static boolean checkInputs(int base, String num){
-        if (base == 0) return false;
         String values = "0123456789ABCDEF";
         String baseValues = values.substring(0, base);
-        if (base == 1) baseValues = "1";
         String[] numberList = new String[num.length()];
         for (int i = 0; i < numberList.length; i++){
             numberList[i] = num.substring(i, i + 1);
@@ -127,18 +141,23 @@ public class NumberConverter {
         return true;
     }
 
+    public static boolean numIsInRange(int min, int max, String num){
+        String numbers = "1234567890";
+        for (int i = 0; i < num.length(); i++){
+            if (numbers.indexOf(num.substring(i, i + 1)) == -1) return false;
+        }
+        if (Integer.parseInt(num) > max) return false;
+        else if (Integer.parseInt(num) < min) return false;
+        return true;
+    }
+
     public String toString(){
-        if (base == 2){
-            int decimal = convertListToInt(convertToDecimal());
-            return "\nOctal Number: " + convertToOctal() + "\nDecimal Number: " + decimal;
-        }
-        if (base == 8){
-            int decimal = convertListToInt(convertToDecimal());
-            return "\nBinary Number: " + convertToBinary() + "\nDecimal Number: " + decimal;
-        }
-        else {
-            return "\nBinary Number: " + convertToBinary() + "\nOctal Number: " + convertToOctal();
-        }
+        String string = "";
+        if (base != 2) string += "Binary Value: " + convertDecimalToBaseX(Integer.parseInt(convertStringListToString(convertToDecimal())), 2) + "\n";
+        if (base != 8) string += "Octal Value: " + convertDecimalToBaseX(Integer.parseInt(convertStringListToString(convertToDecimal())), 8) + "\n";
+        if (base != 10) string += "Decimal Value: " + convertDecimalToBaseX(Integer.parseInt(convertStringListToString(convertToDecimal())), 10) + "\n";
+        if (base != 16) string += "Hex Value: " + convertDecimalToBaseX(Integer.parseInt(convertStringListToString(convertToDecimal())), 16);
+        return string;
     }
 }
 
